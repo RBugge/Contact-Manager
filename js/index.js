ca93d9762f678c23cf4f49661198c6be17a0a7df
rbugge <ryan.bugge@tuta.io>
2021-09-01 20:21:56 -0400

    Login/signup screen

--- js/index.js
+++ js/index.js
@@ -1,24 +1,52 @@
-let contact = `<div class="card border-0 h-100 g-0 mt-2 ms-2" style="max-width: 408px;">
-<div class="row g-0" id="contact-list">
+let contact = `<div class="card border-0 h-100 g-0 mt-2 ms-2">
+<div class="container-fluid d-inline-flex">
+    <img src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+        class="img rounded-start d-inline-flex" alt="..."
+        style="min-height: 110px; max-height: 110px; max-width: 110px; ">
 
-    <div class="col-4">
-        <img src="images/SeekPng.com_avatar-png_1150053.png"
-            class="img-fluid rounded-start" alt="..." style="min-height: 136px; max-height: 136px; max-width: 136px; ">
-    </div>
+    <div class="ms-2 mt-1">
+        <h5 class="card-title">John Doe</h5>
 
-    <div class="col-8">
-        <div class="card-body">
-            <h5 class="card-title">John Doe</h5>
-            <p class="card-text">
-                UCF<br>
-                555-555-5555<br>
-                John@example.com
-            </p>
+        <div class="card-text">
+            555-555-5555<br />
+            John@example.com<br />
+            <div>
+                <i class="bi-tags-fill text-muted"></i>
+            </div>
         </div>
     </div>
-
 </div>
 </div>`;
+
+$(window).ready(function(){
+    $('#login-modal').modal('toggle');
+});
+
+$("#login-btn").on('click', function(){
+    login();
+});
+
+$("#signup-btn").on('click', function(){
+    $('#signup-modal').modal('toggle');
+    login();
+});
+
+function login() {
+    // Test code
+    // Adding cards
+    for (let i = 0; i < 20; i++) {
+        $("#contact-list").append(contact);
+    }
+    // End test code
+    $('#login-modal').modal('toggle');
+    toggleBlur();
+
+}
+
+function toggleBlur() {
+    $("#navbar").toggleClass("modal-blur");
+    $("#card-deck").toggleClass("modal-blur");
+}
 
 function addContact() {
     $("#contact-list").prepend(contact);
@@ -26,6 +54,6 @@ function addContact() {
 }
 
 function scrollTop() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $("html, body").animate({ scrollTop: 0 }, "fast");
     return false;
 };
